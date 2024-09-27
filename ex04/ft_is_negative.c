fbe264c2aa28037b64446dff51e5dc352a22f494
Helder <helderc299@gmail.com>
2024-09-27 03:50:43 +0000

    updating

--- ex04/ft_is_negative.c
+++ ex04/ft_is_negative.c
@@ -5,12 +5,12 @@ char r ;
 if (n <0 ){
 r = 'N';
 write (1, &r,1);}
-else if(n>0){
+else if(n>0) || (n==0){
 r = 'P';
 write (1, &r, 1);
 }}
 
 int main(){
-int numero = 2;
+int numero = 0;
 ft_is_negative(numero);
 return 0;}
